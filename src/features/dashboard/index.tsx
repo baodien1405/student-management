b31d38f62b6af0df3f39a72e5951c59a22a45852
Dien Cap <baodien1405@gmail.com>
2021-08-03 17:02:40 +0700

    Remove console log

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -5,21 +5,28 @@ import React, { useEffect } from 'react';
 import StatisticItem from './components/StatisticItem';
 import StudentRankingList from './components/StudentRankingList';
 import Widget from './components/Widget';
-import { dashboardActions, selectDashboardLoading, selectDashboardStatistics, selectHighestStudentList, selectLowestStudentList, selectRankingByCityList } from './dashboardSlice';
+import {
+  dashboardActions,
+  selectDashboardLoading,
+  selectDashboardStatistics,
+  selectHighestStudentList,
+  selectLowestStudentList,
+  selectRankingByCityList,
+} from './dashboardSlice';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    position: "relative",
-    paddingTop: theme.spacing(1)
+    position: 'relative',
+    paddingTop: theme.spacing(1),
   },
   loading: {
-    position: "absolute",
+    position: 'absolute',
     top: theme.spacing(-1),
-    width: "100%",
-  }
-}))
+    width: '100%',
+  },
+}));
 
-export default function Dashboard () {
+export default function Dashboard() {
   const dispatch = useAppDispatch();
   const loading = useAppSelector(selectDashboardLoading);
   const statistics = useAppSelector(selectDashboardStatistics);
@@ -29,17 +36,9 @@ export default function Dashboard () {
 
   const classes = useStyles();
 
-  console.log({
-    loading,
-    statistics,
-    highestStudentList,
-    lowestStudentList,
-    rankingByCityList
-  })
-
   useEffect(() => {
-    dispatch(dashboardActions.fetchData())
-  }, [dispatch])
+    dispatch(dashboardActions.fetchData());
+  }, [dispatch]);
 
   return (
     <Box className={classes.root}>
@@ -49,25 +48,41 @@ export default function Dashboard () {
       {/* Statistics Section */}
       <Grid container spacing={3}>
         <Grid item xs={12} md={6} lg={3}>
-          <StatisticItem icon={<EmojiPeople fontSize="large" color="primary" />} label="male" value={statistics.maleCount} />
+          <StatisticItem
+            icon={<EmojiPeople fontSize="large" color="primary" />}
+            label="male"
+            value={statistics.maleCount}
+          />
         </Grid>
 
         <Grid item xs={12} md={6} lg={3}>
-          <StatisticItem icon={<PregnantWoman fontSize="large" color="primary" />} label="female" value={statistics.femaleCount} />
+          <StatisticItem
+            icon={<PregnantWoman fontSize="large" color="primary" />}
+            label="female"
+            value={statistics.femaleCount}
+          />
         </Grid>
 
         <Grid item xs={12} md={6} lg={3}>
-          <StatisticItem icon={<CallMade fontSize="large" color="primary" />} label="mark >= 8" value={statistics.highMarkCount} />
+          <StatisticItem
+            icon={<CallMade fontSize="large" color="primary" />}
+            label="mark >= 8"
+            value={statistics.highMarkCount}
+          />
         </Grid>
 
         <Grid item xs={12} md={6} lg={3}>
-          <StatisticItem icon={<CallReceived fontSize="large" color="primary" />} label="mark <= 5" value={statistics.lowMarkCount} />
+          <StatisticItem
+            icon={<CallReceived fontSize="large" color="primary" />}
+            label="mark <= 5"
+            value={statistics.lowMarkCount}
+          />
         </Grid>
       </Grid>
 
       {/* All student by ranking */}
       <Box mt={5}>
-        <Typography variant="h4" >All Students</Typography>
+        <Typography variant="h4">All Students</Typography>
 
         <Box mt={2}>
           <Grid container spacing={3}>
@@ -88,11 +103,11 @@ export default function Dashboard () {
 
       {/* Ranking by city */}
       <Box mt={5}>
-        <Typography variant="h4" >Ranking by city</Typography>
+        <Typography variant="h4">Ranking by city</Typography>
 
         <Box mt={2}>
           <Grid container spacing={3}>
-            {rankingByCityList.map(ranking => (
+            {rankingByCityList.map((ranking) => (
               <Grid item key={ranking.cityId} xs={12} md={6} lg={3}>
                 <Widget title={ranking.cityName}>
                   <StudentRankingList studentList={ranking.rankingList} />
